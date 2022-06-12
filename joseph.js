eef113b49c59a8832a5870ccec0ada6b0e6e85ef
dtk1 <105410205+dtk1@users.noreply.github.com>
2022-06-12 17:40:35 +0600

    Add files via upload

---
+++ joseph.js
@@ -0,0 +1,14 @@
+let songs = [
+{
+    name: 'Ocean Drive',
+    path: 'music/ocean.mp3',
+    artist: 'Duke Dumont',
+    cover: 'images/summer.jpg'
+},
+{
+    name: 'Unforgettable',
+    path: 'music/unfor.mp3',
+    artist: 'French Montana',
+    cover: 'images/summerv.jpg'
+},
+]
